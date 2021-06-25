457dca55cb0931c9c0b779005c07c7a4b5bd8ca6
Ev Sivkova <ev.sivkova@outlook.com>
2021-06-25 11:32:44 -0700

    fixed mutations plain

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -21,26 +21,27 @@ function checkValue($value): string
     return is_array($value) ? '[complex value]' : formatValue($value);
 }
 
-function plain(array $tree, array $rootPath = []): string
+function plain(array $tree, $rootPath = null): string
 {
     $result =  array_map(function ($node) use ($rootPath): string {
-        $rootPath[] = getName($node);
+        $name = getName($node);
+        $path = isset($rootPath) ? implode('.', [$rootPath, $name]) : $name;
         $noChanges = $node['noChanges'];
         if (isValueSet($noChanges) && is_array($noChanges)) {
-            return plain($noChanges, $rootPath);
+            return plain($noChanges, $path);
         }
         $valueBefore = $node['before'];
         $valueAfter = $node['after'];
-        $path = implode('.', $rootPath);
+        //$path = implode('.', $rootPath);
         if (isValueSet($valueBefore) && isValueSet($valueAfter)) {
             $value1 = checkValue($valueBefore);
             $value2 = checkValue($valueAfter);
-            return "Property '$path' was updated. From $value1 to $value2";
+            return  "Property '$path' was updated. From $value1 to $value2";
         } elseif (isValueSet($valueBefore)) {
-            return "Property '$path' was removed";
+            return  "Property '$path' was removed";
         } elseif (isValueSet($valueAfter)) {
             $addedValue = checkValue($valueAfter);
-            return "Property '$path' was added with value: $addedValue";
+            return  "Property '$path' was added with value: $addedValue";
         };
         return '';
     }, $tree);
