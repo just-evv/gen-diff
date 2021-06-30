3f1d4b0e581a1e5e8cad401ae715b831e8762920
Ev Sivkova <ev.sivkova@outlook.com>
2021-06-29 23:11:55 -0700

    refactored

--- src/CompareFiles.php
+++ src/CompareFiles.php
@@ -5,7 +5,37 @@ declare(strict_types=1);
 namespace Gendiff\CompareFiles;
 
 use function Functional\sort;
-use function Gendiff\DiffNode\createNode;
+//use function Gendiff\DiffNode\createNode;
+
+function createNode(string $name, array $children = [], string $type = 'no changes', $value1 = '', $value2 = ''): array
+{
+    return ['name' => $name, 'type' => $type, 'children' => $children, 'value1' => $value1, 'value2' => $value2];
+}
+
+function getName(array $node): string
+{
+    return $node['name'];
+}
+
+function getType(array $node): string
+{
+    return $node['type'];
+}
+
+function getChildren(array $node): array
+{
+    return $node['children'];
+}
+
+function getValue1(array $node)
+{
+    return $node['value 1'];
+}
+
+function getValue2(array $node)
+{
+    return $node['value 2'];
+}
 
 function compareFiles(array $file1, array $file2): array
 {
@@ -20,14 +50,14 @@ function compareFiles(array $file1, array $file2): array
             if (is_array($file1[$key]) && is_array($file2[$key])) {
                 return createNode($key, compareFiles($file1[$key], $file2[$key]));
             } elseif ($file1[$key] === $file2[$key]) {
-                return createNode($key, $file1[$key]);
+                return createNode($key, [], 'no changes', $file1[$key]);
             } else {
-                return createNode($key, [], $file1[$key], $file2[$key]);
+                return createNode($key, [], 'changed', $file1[$key], $file2[$key]);
             }
         } elseif (array_key_exists($key, $file1)) {
-            return createNode($key, [], $file1[$key]);
+            return createNode($key, [], 'removed', $file1[$key]);
         } elseif (array_key_exists($key, $file2)) {
-            return createNode($key, [], [], $file2[$key]);
+            return createNode($key, [], 'added', $file2[$key]);
         };
         return [];
     }, $allKeys);
--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Gendiff\Formatter\Plain;
 
 use function Functional\flatten;
-use function Gendiff\DiffNode\getName;
-use function Gendiff\DiffNode\isValueSet;
+use function Gendiff\CompareFiles\getName;
+use function Gendiff\CompareFiles\getType;
+use function Gendiff\CompareFiles\getChildren;
 
 function formatValue($value): string
 {
@@ -31,21 +32,20 @@ function plain(array $tree, string $rootPath = null): string
     $result =  array_map(function ($node) use ($rootPath): string {
         $name = getName($node);
         $path = isset($rootPath) ? implode('.', [$rootPath, $name]) : $name;
-        $noChanges = $node['noChanges'];
-        if (isValueSet($noChanges) && is_array($noChanges)) {
-            return plain($noChanges, $path);
-        }
-        $valueBefore = $node['before'];
-        $valueAfter = $node['after'];
-        //$path = implode('.', $rootPath);
-        if (isValueSet($valueBefore) && isValueSet($valueAfter)) {
-            $value1 = checkValue($valueBefore);
-            $value2 = checkValue($valueAfter);
+        $type = getType($node);
+        $children = getChildren($node);
+        if ($type === 'no changes') {
+            if (!empty($children)) {
+                return plain($children, $path);
+            }
+        } elseif ($type === 'changed') {
+            $value1 = checkValue($node['value1']);
+            $value2 = checkValue($node['value2']);
             return  "Property '$path' was updated. From $value1 to $value2";
-        } elseif (isValueSet($valueBefore)) {
+        } elseif ($type === 'removed') {
             return  "Property '$path' was removed";
-        } elseif (isValueSet($valueAfter)) {
-            $addedValue = checkValue($valueAfter);
+        } elseif ($type === 'added') {
+            $addedValue = checkValue($node['value1']);
             return  "Property '$path' was added with value: $addedValue";
         };
         return '';
